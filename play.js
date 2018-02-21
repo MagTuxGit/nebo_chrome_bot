6fe1807eb0403bd51316b4b39f73c7a6f6aa392c
TrAnd <magtuxgit@gmail.com>
2018-02-21 13:51:36 +0200

    gather money

--- play.js
+++ play.js
@@ -1,3 +1,58 @@
+const textGetMoney = "Собрать выручку!";
+const textGetGoods = "Закупить товар";
+
+const pages = {
+	home: 'home',
+	floor: 'floor'
+}
+
+const pagesLinks = {
+    'home' : pages.home,
+    'floo' : pages.floor
+}
+
 window.onload = function() {
-	document.write('Hello world')
+	setCurrentPage();
+	setTimeout(run, 1000);
+}
+
+var currentPage = pages.home;
+
+function setCurrentPage() {
+	var pageLink = window.location.pathname.slice(1,5);
+	currentPage = pagesLinks[pageLink];
+	console.log('current page: ' + currentPage);
+}
+
+function run() {
+	switch (currentPage) {
+		case pages.home:
+			if (getMoney()) { return; }
+			if (getGoods()) { return; }
+			break;
+		case pages.floor:
+			console.log("FLOOR");
+			break;
+	}
+}
+
+function go(link) {
+	if (link) { 
+		link.click(); 
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getMoney() {
+	function itemContainsText(item) { return item.textContent.includes(textGetMoney); }
+	const link = Array.from(document.links).filter(itemContainsText)[0];
+	return go(link)
+}
+
+function getGoods() {
+	function itemContainsText(item) { return item.textContent.includes(textGetGoods); }
+	const link = Array.from(document.links).filter(itemContainsText)[0];
+	return go(link)
 }
