bf16fc2940a18c84c16683e3bcc3ec828fefd98e
TrAnd <magtuxgit@gmail.com>
2018-02-22 12:30:54 +0200

    check icons flow (no steps queue)

--- play.js
+++ play.js
@@ -34,13 +34,9 @@ const pagesLinks = {
 const steps = {
 	start: 'start',
 	pause: 'pause',
-	goGetMoney: 'goGetMoney',
 	getMoney: 'getMoney',
-	goPutGoods: 'goPutGoods',
 	putGoods: 'putGoods',
-	goGetGoods: 'goGetGoods',
 	getGoods: 'getGoods',
-	buyItem: 'buyItem',
 	liftUp: 'liftUp'
 }
 
@@ -56,15 +52,10 @@ function go(link) {
 
 function goUrl(url) { window.location = url; }
 function setNextStep(step) { chrome.storage.local.set({'nextStep': step}); }
-function pause() {
-	console.log('PAUSE 30 sec'); 
-	setNextStep(steps.start);
-	setTimeout(goHome, 30000);
-}
 
 // RUN
 var currentPage = pages.home;
-var nextStep
+var nextStep = steps.start
 
 function readStorageAndRun() {
 	setCurrentPage();
@@ -86,102 +77,6 @@ function setCurrentPage() {
 	console.log('current page: ' + currentPage);
 }
 
-function run() {
-	if (!checkLink(pages.getMoney) && 
-	!checkLink(pages.putGoods) && 
-	!checkLink(pages.getGoods) && 
-	!checkText(textLiftAvailable) && 
-	nextStep != steps.getGoods && 
-	nextStep != steps.liftUp) {
-		nextStep = steps.pause;
-	}
-
-	if (nextStep != steps.getMoney && checkLink(pages.getMoney)) { nextStep = steps.goGetMoney; }
-	else if (nextStep != steps.putGoods && checkLink(pages.putGoods)) { nextStep = steps.goPutGoods; }
-	else if (nextStep != steps.getGoods && checkLink(pages.getGoods)) { nextStep = steps.goGetGoods; }
-
-	if (nextStep == steps.getGoods) {
-		if (currentPage == pages.floor) {
-			nextStep = steps.buyItem;
-		}
-	}
-	if (nextStep == steps.liftUp) {
-		if (currentPage == pages.floor) {
-			nextStep = steps.buyItem;
-		}
-	}
-
-	switch (nextStep) {
-		case undefined:
-			console.log("STEP UNDEFINED");
-			setNextStep(steps.start);
-			goHome();
-			break;
-		case steps.pause:
-			pause();
-			break;
-		case steps.start:
-			console.log("STEP START");
-			setNextStep(steps.goGetMoney);
-			goHome();
-			break;
-		case steps.goGetMoney:
-			console.log("STEP GO GET MONEY");
-			setNextStep(steps.getMoney);
-			goUrl('https://nebo.mobi/floors/0/5');
-			break;
-		case steps.getMoney:
-			console.log("STEP MONEY");
-			if (!getMoney()) { 
-				setNextStep(steps.goPutGoods);
-				goHome();
-			}
-			break;
-		case steps.goPutGoods:
-			console.log("STEP GO PUT GOODS");
-			setNextStep(steps.putGoods);
-			goUrl('https://nebo.mobi/floors/0/3');		
-			break;
-		case steps.putGoods:
-			console.log("STEP PUT GOODS");
-			if (!putGoods()) { 
-				setNextStep(steps.goGetGoods);
-				goHome();
-			}
-			break;
-		case steps.goGetGoods:
-			console.log("STEP GO GET GOODS");
-			setNextStep(steps.getGoods);
-			goUrl('https://nebo.mobi/floors/0/2');		
-			break;
-		case steps.getGoods:
-			console.log("STEP GET GOODS");
-			if (!getGoods()) { 
-				setNextStep(steps.liftUp);
-				goUrl('https://nebo.mobi/lift');		
-			}
-			break;
-		case steps.buyItem:
-			console.log("STEP BUY ITEM");
-			setNextStep(steps.getGoods);
-			buyItem();
-			break;
-		case steps.liftUp:
-			console.log("STEP LIFT UP");
-			if (!liftUp()) {
-				if (!getTips()) {
-					setNextStep(steps.start);
-					goHome();
-				}
-			}
-			break;
-		default:
-			console.log("STEP DEFAULT");
-			setNextStep(steps.start);
-			goHome();
-	}
-}
-
 function goHome() {
 	window.location = 'https://nebo.mobi/home';
 }
@@ -201,6 +96,89 @@ function checkText(text) {
 	function itemContainsText(item) { return item.textContent.includes(text); }
 	const link = Array.from(document.links).filter(itemContainsText)[0];
 	return link != null;	
+}
+
+function run() {
+	switch (nextStep) {
+		case undefined:
+			console.log("STEP UNDEFINED");
+			setNextStep(steps.start);
+			goHome();
+			break;
+		case steps.pause:
+			console.log('PAUSE 30 sec'); 
+			setNextStep(steps.start);
+			setTimeout(goHome, 30000);
+			break;
+		case steps.start:
+			console.log("STEP START");
+			if (!checkIcons()) {
+				setNextStep(steps.pause);
+				goHome();
+			}
+			break;
+		case steps.getMoney:
+			console.log("STEP MONEY");
+			if (!getMoney()) { 
+				setNextStep(steps.start);
+				goHome();
+			}
+			break;
+		case steps.putGoods:
+			console.log("STEP PUT GOODS");
+			if (!putGoods()) { 
+				setNextStep(steps.start);
+				goHome();
+			}
+			break;
+		case steps.getGoods:
+			console.log("STEP GET GOODS");
+			if (!getGoods()) { 
+				if (!buyItem()) {
+					setNextStep(steps.start);
+					goHome();
+				}
+			}
+			break;
+		case steps.liftUp:
+			console.log("STEP LIFT UP");
+			if (!liftUp()) {
+				if (!getTips()) {
+					setNextStep(steps.start);
+					goHome();
+				}
+			}
+			break;
+		default:
+			console.log("STEP DEFAULT");
+			setNextStep(steps.start);
+			goHome();
+	}
+}
+
+function checkIcons() {
+	if (checkLink(pages.getMoney)) {
+		setNextStep(steps.getMoney);
+		goUrl('https://nebo.mobi/floors/0/5');
+		return true;
+	}
+	if (checkLink(pages.putGoods)) {
+		setNextStep(steps.putGoods);
+		goUrl('https://nebo.mobi/floors/0/3');		
+		return true;
+	}
+	if (checkLink(pages.getGoods)) {
+		setNextStep(steps.getGoods);
+		goUrl('https://nebo.mobi/floors/0/2');		
+		return true;
+	}
+	if (checkText(textLiftAvailable)) {
+		setNextStep(steps.liftUp);
+		goUrl('https://nebo.mobi/lift');		
+		return true;
+	}
+
+	return false;
 }
 
 function getMoney() { return clickFirstLink(textGetMoney); }
